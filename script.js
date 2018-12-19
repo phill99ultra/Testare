672bde2ce3ede520f815697770f577e38ee67e5b
phill99ultra <38920393+phill99ultra@users.noreply.github.com>
2018-12-19 21:40:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -13,24 +13,22 @@ var json = (function () {
  })();
 
 var localStorageAdd = function(){
-    localStorage.setItem('Date_Base', JSON.stringify(json));
+    localStorage.setItem('date_base', JSON.stringify(json));
 }
 localStorageAdd();
 
-var dataTaken = function(){
-    JSON.parse(localStorage.getItem("Data_Base"));
-}
+var dataTaken = JSON.parse(localStorage.getItem("date_base"));
 
-var completePosts = function(){
-    var post = document.getElementById('posts');
+var complete_posts = function(){
+    var post = document.getElementById("posts");
     post.innerHTML = '';
     for(var i in dataTaken){
-          newPost(i);
+          new_post(i);
     }
 }
 
-var newPost = function(number){
-    var post = document.getElementById('posts');
+var new_post = function(number){
+    var article = document.getElementById("posts");
 
     var tags = function(num){
         tagButton = '';
@@ -39,8 +37,8 @@ var newPost = function(number){
         }
         return tagButton;
     }
-    var button_T = tags(number);
-    post.innerHTML += `<article>
+    var button_t = tags(number);
+    article.innerHTML += `<article>
                          <header>
                            <h3>${dataTaken[number].title}</h3>
                          </header>
@@ -48,11 +46,43 @@ var newPost = function(number){
                            <p>${dataTaken[number].body}</p>
                          </section>
                          <footer>
-                           <div class="tags">${button_T}</div>
+                           <div class="tags">${button_t}</div>
                          </footer>
                          <div class="controls">
-                           <button class="btn btn-danger btn-mini" onclick="deleteArticle(${number})">удалить</button>
+                           <button class="btn btn-danger btn-mini" onclick="delete_tag(${number})">удалить</button>
                          </div>
                        </article>`;
 }
-completePosts();
+complete_posts();
+
+function delete_tag(number) {                                                                 
+    dataTaken.splice(number, 1);
+    localStorage.setItem("date_base", JSON.stringify(dataTaken));
+    complete_posts();
+ }
+
+ var add_new_post = function(post){
+    var title = post.title.value;
+    var body = post.body.value;
+    var tags = post.tags.value;
+    tags = tags.split(",");
+       var reg = /^[а-яА-ЯёЁa-zA-Z 0-9]+$/;
+       if (title.length < 5 && !reg.test(title)) {
+          return false;
+       }
+       if (body.length < 30 && !reg.test(body)) {
+          return false;
+       }
+       if (tags.length < 1 && !reg.test(tags)) {
+          return false;
+       }
+
+       dataTaken[id] += 1;
+       dataTaken[title] += title;
+       dataTaken[body] += body;
+       dataTaken[tags] += tags;
+
+      dataTaken.push(post);
+      localStorage.setItem("Data_Base", JSON.stringify(dataTaken));
+      completePosts();
+  }
