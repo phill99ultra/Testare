649e3bd1b63aac29620009c44e4ae7a9fffe8211
phill99ultra <38920393+phill99ultra@users.noreply.github.com>
2019-01-04 10:32:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,70 +1,67 @@
 
- var json = [];
+ var json = [];  
  function getData(){
-     $.get("json/posts.json")
-       .done(function(data){
-           json = data;
-           localStorageAdd();
-           console.log('DONE!')
+     $.get("json/posts.json")          
+       .done(function(data){       
+           json = data;             
+           localStorageAdd();        
+           console.log('DONE!')     
      })
        .fail(function(data){
-           console.error('Fail!');
+           console.error('Fail!');    
        })
  }
 
  var formAddPost = document.getElementById('post-add');
- var post = document.getElementById("posts");
+ var post = document.getElementById('posts');  
  var buttonAddPost = formAddPost.getElementsByTagName('button')[0];  
  
  function init() {
-     if(localStorage.getItem('date_base')){
-         parsing();
-         complete_posts();
+     if(localStorage.getItem('date_base')){  
+         parsing();                    
+         complete_posts();              
      } else {
-         getData();
-         setTimeout(() => {
-            parsing();
-            complete_posts();
-            console.log("complete");
+         getData();              
+         setTimeout(() => { 
+            parsing();           
+            complete_posts();    
+            console.log("complete");  
           }, 5000);
      }
-     
-    
-        buttonAddPost.addEventListener("click", function (event) {  
-        event.preventDefault();    //previne submit
-        //alert('New Post Added!');   //
-        addNewPost();
+        buttonAddPost.addEventListener("click", function (event) { 
+        event.preventDefault();   
+        //alert('New Post Added!');   
+        addNewPost();   
      });
   }
  
- var localStorageAdd = function(){
+ var localStorageAdd = function(){    
      localStorage.setItem('date_base', JSON.stringify(json));
  }
  
- var parsing = function(){
+ var parsing = function(){    
    data_taken = JSON.parse(localStorage.getItem("date_base"));
  }
- var data_taken = '';
+ var data_taken = '';  
  
- var complete_posts = function(){
+ var complete_posts = function(){   
      post.innerHTML = '';
-     for(var i in data_taken){
-           new_post(i);
+     for(var i in data_taken){  
+           new_post(i);  
      }
  }
  
- var new_post = function(number){
-     var article = document.getElementById("posts");
- 
-     var tags = function(num){
+ var new_post = function(number){  
+     var article = document.getElementById("posts");    
+     var tags = function(num){  
          tag_button = '';
-         for(var tag in data_taken[num].tags){
+         for(var tag in data_taken[num].tags){ 
             tag_button += `<button class="btn btn-xs btn-default">${data_taken[number].tags[tag]}</button>`;
-         }
+         } 
          return tag_button;
      }
-     var button_t = tags(number);
-     article.innerHTML += `<article>
+     var button_t = tags(number);   
+     article.innerHTML += `<article>   
                           <header>
                             <h3>${data_taken[number].title}</h3>
                           </header>
@@ -77,30 +74,30 @@
                           <div class="controls">
                             <button class="btn btn-danger btn-mini" onclick="delete_tag(${number})">удалить</button>
                           </div>
-                        </article>`;
+                        </article>`; 
  }
  
- function delete_tag(number) {                                                                 
-     dataTaken.splice(number, 1);
+ function delete_post(number) {                                                     
+     data_taken.splice(number, 1);    
      localStorage.setItem("date_base", JSON.stringify(data_taken));
-     complete_posts();
+     complete_posts();   
   } 
  
-  function addNewPost() {
-     var title;
+  function addNewPost() {  
+     var title;    
      var body;
      var tags;
-     var formInputs;
-     var newPost = {
+     var formInputs;  
+     var newPost = {  
          id: data_taken.length + 1,
         title: '',
         body: '',
         tags: []
      };
   
-     formInputs = formAddPost.querySelectorAll('[name]');
+     formInputs = formAddPost.querySelectorAll('[name]'); 
   
-     for (var input of formInputs) {
+     for (var input of formInputs) {  
         if (input.name == 'title') {
            title = input.value;
         }
@@ -111,22 +108,22 @@
            tags = input.value;
         }
      }
-     newPost.title = title;
-     newPost.body = body;
-     newPost.tags = tags.split(",");
-     var reg = /^[а-яА-ЯёЁa-zA-Z 0-9]+$/;
+     newPost.title = title;   
+     newPost.body = body;     
+     newPost.tags = tags.split(","); 
+     var reg = /^[а-яА-ЯёЁa-zA-Z 0-9]+$/;  
           if (title.length < 5 && !reg.test(title)) {
-             return false;
-          }
+             return false; 
+          }                 
           if (body.length < 30 && !reg.test(body)) {
              return false;
           }
           if (tags.length < 1 && !reg.test(tags)) {
              return false;
           }
-     //console.log(newPost);
-     data_taken.push(newPost);
-     localStorage.setItem("date_base", JSON.stringify(data_taken));
-     complete_posts()
+     //console.log(newPost); 
+     data_taken.push(newPost);  
+     localStorage.setItem("date_base", JSON.stringify(data_taken)); 
+     complete_posts()  
  }
- init();
+ init();  
